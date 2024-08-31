744869ec9af7866abb0734e219be538f85ee6204
coollogan876 <119338946+coollogan876@users.noreply.github.com>
2024-08-30 23:33:34 -0500

    Added PID objects

--- HighStakes/src/comp.cpp
+++ HighStakes/src/comp.cpp
@@ -1,22 +1,33 @@
 #include "vex.h"
+#include "pid.h"
 
 using namespace vex;
 
 void drive(void)
 {
+    thread lock = thread(lock);
+
     Drivetrain.setDriveVelocity(100, percent);
     Drivetrain.setStopping(brake);
     while (true)
     {
-        leftMotors.setVelocity(Controller.Axis3.position() + Controller.Axis2.position(), percent);
-        rightMotors.setVelocity(Controller.Axis3.position() - Controller.Axis2.position(), percent);
-        leftMotors.spin(forward);
-        rightMotors.spin(forward);
+        leftMotorA.setVelocity(Controller.Axis3.position() + Controller.Axis1.position(), percent);
+        leftMotorB.setVelocity(Controller.Axis3.position() + Controller.Axis1.position(), percent);
+        rightMotorA.setVelocity(Controller.Axis3.position() - Controller.Axis1.position(), percent);
+        rightMotorB.setVelocity(Controller.Axis3.position() - Controller.Axis1.position(), percent);
+        leftMotorA.spin(forward);
+        leftMotorB.spin(forward);
+        rightMotorA.spin(forward);
+        rightMotorB.spin(forward);
         wait(10, msec);
     }
 }
 
 void auton(void)
-{
-    PIDdrive(300, 0);
+{    
+    pid piddrive = pid(leftMotors, rightMotors, 3.25, 1.145, 0.065, 0.15, 2.25);
+    pid pidturn = pid(Inertial, leftMotors, rightMotors, 3.25, 0.15, 0.0, 0.0, 2.25);
+    piddrive.enable = true;
+    //piddrive.move(5, forward);
+    piddrive.turn(90, left);
 }
