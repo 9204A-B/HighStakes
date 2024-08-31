1b7ba1556dda47fe97ca1986e200a9196cce3984
coollogan876 <119338946+coollogan876@users.noreply.github.com>
2024-08-30 23:35:35 -0500

    Update pid.cpp

--- HighStakes/src/pid.cpp
+++ HighStakes/src/pid.cpp
@@ -1,92 +1,230 @@
 #include "vex.h"
+#include <math.h>
 
 using namespace vex;
 
-double kp = 0.25;
-double ki = 0.000000005;
-double kd = 0.22;
-double integral = 0.0;
-
-double turnKp = 0.25;
-double turnKi = 0.000000005;
-double turnKd = 0.22;
-double turnIntegral = 0.0;
-
-double integralThreshold = 10.0;
-
-bool PIDenable = true;
-bool resetSensor = false;
-
-void PIDdrive(int desiredValue, int desiredTurnValue)
-{
-    int error;
-    int previousError = 0;
-    int derivitive;
-
-    int turnError;
-    int turnPreviousError = 0;
-    int turnDerivitive;
-
-    PIDenable = true;
-    resetSensor = true;
-
-    while (PIDenable)
+    void pid::move(double desiredValue, directionType direction)
     {
-        if (resetSensor)
-        {
-          //Inertial.calibrate(0);
-            resetSensor = false;
-            leftMotors.setPosition(0, degrees);
-            rightMotors.setPosition(0, degrees);
-        }
-      //int position = Inertial.gyroRate(xaxis, dps);
-        int leftPosition = leftMotors.position(degrees);
-        int rightPosition = rightMotors.position(degrees);
-        int averagePosition = (leftPosition + rightPosition) / 2;
-
-        previousError = error;
-        error = desiredValue - averagePosition;
-      //error = desiredVaule - position;
+        double position = 0.0;
+        double previousPosition = 0.0;
+        double time = Brain.timer(msec);
+        double previousTime = 0.0;
+        double error = 0.0;
+        double previousError = 0.0;
+        double derivitive = 0.0;
+        double integral = 0.0;
         
-        if (abs(error) < integralThreshold)
+        while (enable)
         {
-            integral += error;
+            if (MotorsA != NULL)
+            {
+                position = ((MotorsA->position(degrees) + MotorsB->position(degrees)) / 2.0) / 360.0 * wheelSize * M_PI;
+            }
+            else if (MotorA != NULL)
+            {
+                position = ((MotorA->position(degrees) + MotorB->position(degrees)) / 2.0) / 360.0 * wheelSize * M_PI;
+            }
+            else if (Motor != NULL)
+            {
+                position = Motor->position(degrees);
+            }
+            else
+            {
+                return;
+            }
+
+            if (reset)
+            {
+                if (MotorsA != NULL)
+                {
+                    MotorsA->setPosition(0, degrees);
+                    MotorsB->setPosition(0, degrees);
+                }
+                else if (MotorA != NULL)
+                {
+                    MotorA->setPosition(0, degrees);
+                    MotorB->setPosition(0, degrees);
+                }
+                else 
+                {
+                    Motor->setPosition(0, degrees);
+                }
+                reset = false;
+                position = 0.0;
+            }
+
+            error = desiredValue - position;
+
+            if (fabs(error) < integralThreshold)
+            {
+                integral += error;
+            }
+            else
+            {
+                integral = 0.0;
+            }
+
+            //Prevent dividing by 0
+            if (time == previousTime)
+            {
+                derivitive = 0.0;
+            }
+            else
+            {
+                derivitive = (error - previousError) / (time - previousTime);
+            }
+            previousTime = time;
+
+            double driveMotorPower = kp * error + ki * integral + kd * derivitive;
+
+            if (MotorsA != NULL)
+            {
+                MotorsA->spin(direction, driveMotorPower, volt);
+                MotorsB->spin(direction, driveMotorPower, volt);
+            }
+            else if (MotorA != NULL)
+            {
+                MotorA->spin(direction, driveMotorPower, volt);
+                MotorB->spin(direction, driveMotorPower, volt);
+            }
+            else
+            {
+                Motor->spin(direction, driveMotorPower, volt);
+            }
+
+            if (fabs(position) >= fabs(desiredValue))
+            {
+                enable = false;
+
+                if (MotorsA != NULL)
+                {
+                    MotorsA->stop();
+                    MotorsB->stop();
+                }
+                else if (MotorA != NULL)
+                {
+                    MotorA->stop();
+                    MotorB->stop();
+                }
+                else
+                {
+                    Motor->stop();
+                }
+            }
+
+            wait(15, msec);
         }
-        else
-        {
-            integral = 0;
-        }
-
-        derivitive = error - previousError;
-
-        double driveMotorPower = kp * error + ki * integral + kd * derivitive;
-      //int turnDifference = Inertial.rotation(degrees);
-        int turnDifference = leftPosition - rightPosition;
-
-        turnPreviousError = turnError;
-        turnError = desiredTurnValue - turnDifference;
-
-        if (abs(turnError) < integralThreshold)
-        {
-            turnIntegral += turnError;
-        }
-        else
-        {
-            turnIntegral = 0;
-        }
-
-        turnDerivitive = turnError - turnPreviousError;
-
-        double turnMotorPower = turnKp * turnError + turnKi * turnIntegral + turnKd * turnDerivitive;
-
-        leftMotors.spin(forward, (driveMotorPower + turnMotorPower), volt);
-        rightMotors.spin(forward, (driveMotorPower - turnMotorPower), volt);
-
-        if (averagePosition >= desiredValue && averagePosition >= desiredTurnValue)
-        {
-            PIDenable = false;
-        }
-    
-        wait(15, msec);
     }
-}
+    
+    void pid::turn(double desiredValue, directionType direction)
+    {
+        double rotation = 0.0;
+        double time = Brain.timer(msec);
+        double previousTime = 0.0;
+        double error = 0.0;
+        double previousError = 0.0;
+        double derivitive = 0.0;
+        double integral = 0.0;
+
+        while (enable)
+        {
+            if (sensor != NULL)
+            {
+                while (Inertial.isCalibrating())
+                {
+                    wait(150, msec);
+                }
+                rotation = Inertial.rotation(degrees);    
+            }
+            else if (MotorsA != NULL)
+            {
+                rotation = (MotorsA->position(degrees) + MotorsB->position(degrees)) / 2.0;
+            }
+            else if (MotorA != NULL)
+            {
+                rotation = (MotorA->position(degrees) + MotorB->position(degrees)) / 2.0;
+            }
+            else
+            {
+                return;
+            }
+
+            if (reset)
+            {
+                if (sensor != NULL)
+                {
+                    sensor->calibrate(0);
+                }
+                
+                if (MotorsA != NULL)
+                {
+                    MotorsA->setPosition(0, degrees);
+                    MotorsB->setPosition(0, degrees);
+                }
+                else
+                {
+                    MotorA->setPosition(0, degrees);
+                    MotorB->setPosition(0, degrees);
+                }
+
+                reset = false;
+                rotation = 0.0;
+            }
+
+            error = desiredValue - rotation;
+            if (fabs(error) < integralThreshold)
+            {
+                integral += error;
+            }
+            else
+            {
+                integral = 0.0;
+            }
+
+            if (time == previousTime)
+            {
+                derivitive = 0.0;
+            }
+            else 
+            {
+                derivitive = (error - previousError) / (time - previousTime);
+            }
+
+            previousTime = time;
+
+            double driveMotorPower = kp * error + ki * integral + kd * derivitive;
+            if (MotorsA != NULL)
+            {
+                MotorsA->spin(direction, driveMotorPower, volt);
+                MotorsB->spin(direction, -1 * driveMotorPower, volt);                
+            }
+            else
+            {
+                MotorA->spin(direction, driveMotorPower, volt);
+                MotorB->spin(direction, -1 * driveMotorPower, volt);                
+            }
+
+            if (fabs(rotation) >= fabs(desiredValue))
+            {
+                enable = false;
+
+                if (MotorsA != NULL)
+                {
+                    MotorsA->stop();
+                    MotorsB->stop();
+                }
+                else
+                {
+                    MotorA->stop();
+                    MotorB->stop();
+                }
+            }
+
+            wait(10, msec);
+        }
+    }
+
+    void pid::Reset()
+    {
+        reset = true;
+    }
