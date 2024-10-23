75df26d5d3c10ce1e08e22912066be318b553115
Logan Dresel <coollogan876@gmail.com>
2024-10-23 14:47:39 -0500

    Fixed lock

--- HighStakes/src/goalLock.cpp
+++ HighStakes/src/goalLock.cpp
@@ -7,20 +7,22 @@ void lock()
     bool locked = false;
     while (true)
     {
-        while (!(Controller.ButtonL1.pressing() && goalDetect.objectDistance(mm) <= 55))
-        {
-            wait(5, msec);
+
+            while (!(Controller.ButtonL1.pressing() && goalDetect.objectDistance(mm) <= 55))
+            {
+                wait(5, msec);
+            }
+
+            locked = !locked;
+            mobileGoalLock.set(locked);
+
+            while (Controller.ButtonL1.pressing())
+            {
+                wait(5, msec);
+            }
+
+            wait(10, msec);
         }
-
-        locked = !locked;
-        mobileGoalLock.set(locked);
-
-        while (Controller.ButtonL1.pressing())
-        {
-            wait(5, msec);
-        }
-
-        wait(10, msec);
-    }
+        wait(5, msec);
 }
 
