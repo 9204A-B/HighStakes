cbcff3f0a50110aa28a6b15c8ed99e9a31f38450
Logan Dresel <coollogan876@gmail.com>
2024-10-23 14:48:51 -0500

    Fixed port number not showing

--- HighStakes/src/temp.cpp
+++ HighStakes/src/temp.cpp
@@ -1,20 +1,25 @@
 #include "vex.h"
+#include "array"
 using namespace vex;
 
 void warning()
 {
     motor* motors[] = {&leftMotorA, &leftMotorB, &rightMotorA, &rightMotorB, &firstStageIntake, &secondStageIntake, &armMotor};
+    int ports[] = {11, 12, 19, 20, 13, 21, 4};
 
-    for (motor* Motor : motors)
+    while (true)
     {
-        if (Motor->temperature(celsius) > 55)
+        for (int x = 0; x < 7; x++)
         {
-            Controller.Screen.clearScreen();
-            Controller.Screen.setCursor(1, 1);
-            Controller.Screen.print("Motor port: ", Motor->index());
-            Controller.Screen.print(", temp: %d", Motor->temperature(celsius));
-            Controller.rumble("...--");
+            if (motors[x]->temperature(celsius) > 50)
+            {            
+                Controller.Screen.clearScreen();
+                Controller.Screen.setCursor(1, 1);
+                Controller.Screen.print("Motor port: ", ports[x]);
+                Controller.Screen.print(", temp: %f", motors[x]->temperature(celsius));
+                Controller.rumble("...--");
+            }
         }
-    }
     wait(250, msec);
+    }
 }
