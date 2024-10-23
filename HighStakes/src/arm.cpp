b8aa5950aab33a4fb5ba808da6cfbf89c10227b4
Logan Dresel <coollogan876@gmail.com>
2024-10-23 14:45:14 -0500

    Add arm toggle

--- HighStakes/src/arm.cpp
+++ HighStakes/src/arm.cpp
@@ -3,5 +3,31 @@ using namespace vex;
 
 void armMovement()
 {
-    
+    bool up = false;
+    while (true)
+    {
+
+            while (!(Controller.ButtonL2.pressing()))
+            {
+                wait(5, msec);
+            }
+
+            up = !up;
+            if (up)
+            {
+                armMotor.spinTo(800, degrees, false);
+            }
+            else
+            {
+                armMotor.spinTo(10, degrees, false);
+            }
+
+            while (Controller.ButtonL1.pressing())
+            {
+                wait(5, msec);
+            }
+
+            wait(10, msec);
+        }
+        wait(5, msec);
 }
