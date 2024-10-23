e55cd1979c396b321e3f86f73029980ac7f07f92
Logan Dresel <coollogan876@gmail.com>
2024-10-23 14:47:07 -0500

    reduced steering speed to 80%

--- HighStakes/src/comp.cpp
+++ HighStakes/src/comp.cpp
@@ -8,14 +8,15 @@ void drive(void)
 {
     thread lockThread = thread(lock);
     thread intakeThread = thread(intake);
+    thread armThread = thread(armMovement);
     Drivetrain.setDriveVelocity(100, percent);
     Drivetrain.setStopping(brake);
     while (true)
     {
-        leftMotorA.setVelocity(Controller.Axis3.position() + Controller.Axis1.position(), percent);
-        leftMotorB.setVelocity(Controller.Axis3.position() + Controller.Axis1.position(), percent);
-        rightMotorA.setVelocity(Controller.Axis3.position() - Controller.Axis1.position(), percent);
-        rightMotorB.setVelocity(Controller.Axis3.position() - Controller.Axis1.position(), percent);
+        leftMotorA.setVelocity(Controller.Axis3.position() + Controller.Axis1.position() * .8, percent);
+        leftMotorB.setVelocity(Controller.Axis3.position() + Controller.Axis1.position() * .8, percent);
+        rightMotorA.setVelocity(Controller.Axis3.position() - Controller.Axis1.position() * .8, percent);
+        rightMotorB.setVelocity(Controller.Axis3.position() - Controller.Axis1.position() * .8, percent);
         leftMotorA.spin(forward);
         leftMotorB.spin(forward);
         rightMotorA.spin(forward);
@@ -26,5 +27,8 @@ void drive(void)
 
 void auton(void)
 {   
+    pid piddrive = pid(leftMotors, rightMotors, 3.25, 1.145, 0.065, 0.15, 2.25);
+    pid pidturn = pid(Inertial, leftMotors, rightMotors, 3.25, 0.08, 0.045, 0.02, 2.25);
     LeftSide::run();
+    //add auton selector
 }
