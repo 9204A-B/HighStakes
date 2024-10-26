0378c2c4cd52592fc1b55c1e5a1eb3190ffe0a2e
Logan Dresel <coollogan876@gmail.com>
2024-10-25 22:13:19 -0500

    Changed drivetrain to smartdrive | Updated arm motors

--- HighStakes/src/robot-config.cpp
+++ HighStakes/src/robot-config.cpp
@@ -11,14 +11,16 @@ motor leftMotorB = motor(PORT12, ratio6_1);
 motor rightMotorA = motor(PORT19, ratio6_1, true);
 motor rightMotorB = motor(PORT20, ratio6_1);
 
+inertial Inertial = inertial(PORT1);
 motor_group leftMotors = motor_group(leftMotorA, leftMotorB);
 motor_group rightMotors = motor_group(rightMotorA, rightMotorB);
-drivetrain Drivetrain = drivetrain(leftMotors, rightMotors, 82.55, 387.35, 304.8, mm, 1);
+smartdrive Drivetrain = smartdrive(leftMotors, rightMotors, Inertial, 82.55, 387.35, 304.8, mm, 1);
 
 digital_out mobileGoalLock = digital_out(Brain.ThreeWirePort.A);
 distance goalDetect = distance(PORT2);
+bool locked = false;
 
-inertial Inertial = inertial(PORT1);
+
 
 motor firstStageIntake = motor(PORT13, ratio18_1);
 motor secondStageIntake = motor(PORT21, ratio6_1, true);
@@ -27,11 +29,15 @@ int intakeSpeed = 93;
 bool intakeForward = false;
 bool intakeReverse = false;
 
-motor armMotor = motor(PORT4, ratio36_1);
+motor armMotorA = motor(PORT4, ratio36_1, true);
+motor armMotorB = motor(PORT5, ratio36_1);
+motor_group armMotors = motor_group(armMotorA, armMotorB);
 
 competition Competition;
+digital_in button = digital_in(Brain.ThreeWirePort.B);
+int autonSelect = 0;
 
 void vexcodeInit(void)
 {
-    //Vex requires this to compile    
+    //Vex requires this to compile   
 }
