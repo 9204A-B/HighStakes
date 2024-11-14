c9040f3e7f26556b8315ce83d6db7d93c483dc6f
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-11-14 10:55:01 -0600

    Nuke the random wait()

--- HighStakes/src/goalLock.cpp
+++ HighStakes/src/goalLock.cpp
@@ -7,21 +7,20 @@ void lock()
     while (true)
     {
 
-            while (!(Controller.ButtonL1.pressing() && goalDetect.objectDistance(mm) <= clampDistance))
-            {
-                wait(5, msec);
-            }
-
-            locked = !locked;
-            mobileGoalLock.set(locked);
-
-            while (Controller.ButtonL1.pressing())
-            {
-                wait(5, msec);
-            }
-
-            wait(10, msec);
+        while (!(Controller.ButtonL1.pressing() && goalDetect.objectDistance(mm) <= clampDistance))
+        {
+            wait(5, msec);
         }
-        wait(5, msec);
+
+        locked = !locked;
+        mobileGoalLock.set(locked);
+
+        while (Controller.ButtonL1.pressing())
+        {
+            wait(5, msec);
+        }
+
+        wait(10, msec);
+    }
 }
 
