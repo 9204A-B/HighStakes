523f058dff9666614b3b1e820dd5393b66523d3a
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-11-15 19:08:09 -0600

    Optimize code

--- HighStakes/src/comp.cpp
+++ HighStakes/src/comp.cpp
@@ -13,18 +13,10 @@ void drive(void)
     Drivetrain.setStopping(brake);
     while (true)
     {
-        leftMotorA.setVelocity(Controller.Axis3.position() + Controller.Axis1.position() * .8, percent);
-        leftMotorB.setVelocity(Controller.Axis3.position() + Controller.Axis1.position() * .8, percent);
-        leftMotorC.setVelocity(Controller.Axis3.position() + Controller.Axis1.position() * .8, percent);
-        rightMotorA.setVelocity(Controller.Axis3.position() - Controller.Axis1.position() * .8, percent);
-        rightMotorB.setVelocity(Controller.Axis3.position() - Controller.Axis1.position() * .8, percent);
-        rightMotorC.setVelocity(Controller.Axis3.position() - Controller.Axis1.position() * .8, percent);
-        leftMotorA.spin(forward);
-        leftMotorB.spin(forward);
-        leftMotorC.spin(forward);
-        rightMotorA.spin(forward);
-        rightMotorB.spin(forward);
-        rightMotorC.spin(forward);
+        leftMotors.setVelocity(Controller.Axis3.position() + Controller.Axis1.position() * .8, percent);
+        rightMotors.setVelocity(Controller.Axis3.position() - Controller.Axis1.position() * .8, percent);
+        leftMotors.spin(forward);
+        rightMotors.spin(forward);
         wait(10, msec);
     }
 }
