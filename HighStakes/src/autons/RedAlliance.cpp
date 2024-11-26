7793af9f9e9e2d7500e81e2dffd644b3485a5c08
KjEndurance <endurance.tem@gmail.com>
2024-11-25 21:25:05 -0500

    fixed red alliance turning functions and center ring grabbing. will fix for blue alliance and auton skills after may also offset the auton so that we can start directly facing the goal.

--- HighStakes/src/autons/RedAlliance.cpp
+++ HighStakes/src/autons/RedAlliance.cpp
@@ -1,5 +1,20 @@
-#include "vex.h";
+#include "vex.h"
+#inlcude <math.h>
 using namespace vex;
+
+// in JAR template, positive angles -> clockwise, negative -> counter-clockwise
+// if we want the robot to start with the clamp already pointed at the goal, subtract 30 from all .turn_to_angle() functions.
+// i might just do that after changing out all the turning functions, its a pretty easy fix.
+
+// these are values for grabbing the two rings on the auton line.
+float clusterTarget = 3.5; // this is currently aimed right at the center of the ring. increase to make it further from where the two rings touch.
+// 3.5 aims for dead-center on the ring. tweak this until the other ring doesn't get knocked too badly
+
+float clusterAngleRad = atan(clusterTarget / 24); 
+float clusterAngle = clusterAngleRad * (360 / 3.141); // this is in degrees, c++ trig uses radians
+
+float clusterDist = (clusterTarget / sin(clusterAngleRad)) - 0; // tweak this until the robot doesn't go over the auton line
+// you should only be tweaking clusterTarget and clusterDist
 
 namespace Autons
 {
@@ -14,7 +29,12 @@ namespace Autons
             // gets 4 rings onto a mobile goal
             // ends in positive corner
 
+            // in JAR template, positive angles -> clockwise, negative -> counter-clockwise
+            // if we want the robot to start with the clamp already pointed at the goal, subtract 30 from all .turn_to_angle functions.
+            // i might just do that after changing out all the turning functions, its a pretty easy fix.
+
             // back into the mobile goal and drop match load ring
+            pidDrivetrain.turn_to_angle(30);
             pidDrivetrain.drive_distance(-41.6);
             mobileGoalLock.set(true);
             intakeMotors.setVelocity(intakeSpeed, percent);
@@ -22,28 +42,32 @@ namespace Autons
             wait(500);
 
             // turn and drive forwards for ring 2
-            pidDrivetrain.right_swing_to_angle(60);
-            pidDrivetrain.drive_distance(24 + (7 / 5));
+            pidDrivetrain.turn_to_angle(90);
+            pidDrivetrain.drive_distance(24);
             wait(500);
 
+            // update: i've decided to just turn and drive towards the ring from the previous ring's position.
+            // it's painfully slow, but that's the best i can do without access to the robot
+
             // turn and drive into ring 3 on the line
-            pidDrivetrain.right_swing_to_angle(90);
-            pidDrivetrain.drive_distance(24 - (15 / 2)); // tweak this until the robot doesn't go over the auton line
+            pidDrivetrain.turn_to_angle(180 + clusterAngle);
+            pidDrivetrain.drive_distance(clusterDist); 
             wait(500);
 
             // turn and drive into ring 4
-            pidDrivetrain.drive_distance(-12);
-            pidDrivetrain.left_swing_to_angle(30);
-            pidDrivetrain.drive_distance(14);
+            pidDrivetrain.drive_distance(-1 * clusterDist);
+            pidDrivetrain.turn_to_angle(180 - clusterAngle);
+            pidDrivetrain.drive_distance(clusterDist);
             wait(500);
-            pidDrivetrain.drive_distance(-14);
+            pidDrivetrain.drive_distance(-1 * clusterDist);
             intakeMotors.stop();
 
             // turning and driving into the corner
-            pidDrivetrain.right_swing_to_angle(180 - 64);
-            pidDrivetrain.drive_distance(-130)
+            pidDrivetrain.turn_to_angle(291.8);
+            pidDrivetrain.drive_distance(129.2 - 0); // tweak this until the robot doesn't completely ram into the corner
+            // this auton ends with the intake facing the corner. can be easily changed.
 
-                break;
+            break;
         }
         case Autons::Route::fourRingMogo_Ladder:
         {
@@ -52,77 +76,40 @@ namespace Autons
             // ends touching ladder
 
             // back into the mobile goal and drop match load ring
-            pidDrivetrain.drive_distance(-41.6); // this number can be made smaller if the robot doesn't start in the middle of the line
+            pidDrivetrain.turn_to_angle(30);
+            pidDrivetrain.drive_distance(-41.6);
             mobileGoalLock.set(true);
             intakeMotors.setVelocity(intakeSpeed, percent);
             intakeMotors.spin(forward);
             wait(500);
 
             // turn and drive forwards for ring 2
-            pidDrivetrain.right_swing_to_angle(60);
-            pidDrivetrain.drive_distance(24 + (7 / 5));
+            pidDrivetrain.turn_to_angle(90);
+            pidDrivetrain.drive_distance(24);
             wait(500);
 
+            // update: i've decided to just turn and drive towards the ring from the previous ring's position.
+            // it's painfully slow, but that's the best i can do without access to the robot
+
             // turn and drive into ring 3 on the line
-            pidDrivetrain.right_swing_to_angle(90);
-            pidDrivetrain.drive_distance(24 - (15 / 2)); // tweak this until the robot doesn't go over the auton line
+            pidDrivetrain.turn_to_angle(180 + clusterAngle);
+            pidDrivetrain.drive_distance(clusterDist); 
             wait(500);
 
             // turn and drive into ring 4
-            pidDrivetrain.drive_distance(-12);
-            pidDrivetrain.left_swing_to_angle(30);
-            pidDrivetrain.drive_distance(14);
+            pidDrivetrain.drive_distance(-1 * clusterDist);
+            pidDrivetrain.turn_to_angle(180 - clusterAngle);
+            pidDrivetrain.drive_distance(clusterDist);
             wait(500);
-            pidDrivetrain.drive_distance(-14);
+            pidDrivetrain.drive_distance(-1 * clusterDist);
             intakeMotors.stop();
 
-            // turn towards ladder and touch
-            pidDrivetrain.swing_right_to_angle(180 - 77);
-            pidDrivetrain.drive_distance(53);
+            // turning and driving into the corner
+            pidDrivetrain.turn_to_angle(270);
+            pidDrivetrain.drive_distance(24) // tweak until the robot doesn't break itself on the ladder
 
             break;
-        }
-        case Autons::Route::mogoSide_Corner:
-        {
-            // starting on the left
 
-            // back into the mobile goal and drop match load ring
-            pidDrivetrain.drive_distance(-41.6);
-            mobileGoalLock.set(true);
-            intakeMotors.setVelocity(intakeSpeed, percent);
-            intakeMotors.spin(forward);
-            wait(500);
-
-            // turn and drive forwards for ring 2
-            pidDrivetrain.left_swing_to_angle(60);
-            pidDrivetrain.drive_distance(36);
-            wait(500);
-            intakeMotors.stop();
-
-            // turn and drive into the corner head-first
-            pidDrivetrain.right_swing_to_angle(90);
-            pidDrivetrain.drive_distance(70);
-        }
-        case Autons::Route::mogoSide_Ladder:
-        {
-            // starting on the left
-
-            // back into the mobile goal and drop match load ring
-            pidDrivetrain.drive_distance(-41.6);
-            mobileGoalLock.set(true);
-            intakeMotors.setVelocity(intakeSpeed, percent);
-            intakeMotors.spin(forward);
-            wait(500);
-
-            // turn and drive forwards for ring 2
-            pidDrivetrain.right_swing_to_angle(60);
-            pidDrivetrain.drive_distance(36);
-            wait(500);
-            intakeMotors.stop();
-
-            // turn and drive head-first into ladder
-            pidDrivetrain.right_swing_to_angle(180);
-            pidDrivetrain.drive_distance(60);
         }
         }
     }
