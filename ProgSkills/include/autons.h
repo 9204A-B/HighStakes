927b4522f1a766c6e86d6af789f92a31c6e5b455
KjEndurance <endurance.tem@gmail.com>
2024-12-09 13:30:42 -0600

    removed match autons from ProgSkills

--- ProgSkills/include/autons.h
+++ ProgSkills/include/autons.h
@@ -10,35 +10,6 @@ void default_constants();
 
 namespace Autons
 {
-    enum Color
-    {
-        red,
-        blue
-    };
-
-    enum Route
-    {
-        fourRingMogo_Corner,
-        fourRingMogo_Ladder,
-        mogoSide_Corner,
-        mogoSide_Ladder,
-        mogoSide_GoalRush_Corner,
-        mogoSide_GoalRush_Ladder,
-        test
-    };
-
-    class RedAlliance
-    {
-    public:
-        static void run(Autons::Route Route);
-    };
-
-    class BlueAlliance
-    {
-    public:
-        static void run(Autons::Route Route);
-    };
-
     class Skills
     {
     public:
