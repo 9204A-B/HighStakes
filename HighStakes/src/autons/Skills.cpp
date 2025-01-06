9219c966d7b89258e5ecd282e53726f9c2819896
KjEndurance <endurance.tem@gmail.com>
2025-01-06 00:37:40 -0600

    Prog Skills finished on the first half 6 rings on each mobile goal in each corner Will need to tweak the delays and timings when picking up rings (a lot)

--- HighStakes/src/autons/Skills.cpp
+++ HighStakes/src/autons/Skills.cpp
@@ -39,11 +39,12 @@ namespace Autons
         pidDrivetrain.drive_distance(26.8);
 
         // 3 rings on left corner
+        pidDrivetrain.turn_to_angle(0);
+        pidDrivetrain.drive_distance(48);
+
         pidDrivetrain.drive_max_voltage = 8;
         pidDrivetrain.turn_max_voltage = 8;
 
-        pidDrivetrain.turn_to_angle(0);
-        pidDrivetrain.drive_distance(48);
         pidDrivetrain.turn_to_angle(-45);
         pidDrivetrain.drive_distance(17);
         pidDrivetrain.turn_to_angle(180);
@@ -69,5 +70,40 @@ namespace Autons
         pidDrivetrain.drive_max_voltage = 6; // dropping the speed so that the goal doesn't get knocked away
 
         pidDrivetrain.drive_distance(-12);
+        mobileGoalLock.set(true);
+
+        // 3 rings further away on the right
+        pidDrivetrain.drive_max_voltage = 12;
+
+        pidDrivetrain.turn_to_angle(180);
+        pidDrivetrain.drive_distance(24);
+        pidDrivetrain.turn_to_angle(-90);
+        pidDrivetrain.drive_distance(24);
+        pidDrivetrain.turn_to_angle(180 + 26.6);
+        pidDrivetrain.drive_distance(26.8);
+
+        // 3 rings on right corner
+        pidDrivetrain.turn_to_angle(0);
+        pidDrivetrain.drive_distance(48);
+
+        pidDrivetrain.drive_max_voltage = 8;
+        pidDrivetrain.turn_max_voltage = 8;
+
+        pidDrivetrain.turn_to_angle(45);
+        pidDrivetrain.drive_distance(17);
+        pidDrivetrain.turn_to_angle(180);
+        pidDrivetrain.drive_distance(12);
+        wait(1000, msec);
+        intakeMotors.stop();
+
+        // drop second goal
+        pidDrivetrain.drive_max_voltage = 12;
+        pidDrivetrain.turn_max_voltage = 12;
+
+        pidDrivetrain.turn_to_angle(90 + 45);
+        pidDrivetrain.drive_distance(-1 * goalDropDist1);
+        mobileGoalLock.set(false);
+        wait(500);
+        pidDrivetrain.drive_distance(goalDropDist1);
     }
 }
