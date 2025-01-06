e5f954e494428406e21c9e75b2c6fa8901023ca8
KjEndurance <endurance.tem@gmail.com>
2025-01-05 23:56:42 -0600

    Started new programming skills route Finished up to scoring onto alliance stake and moving onto the start line.

--- HighStakes/src/autons/Skills.cpp
+++ HighStakes/src/autons/Skills.cpp
@@ -6,5 +6,15 @@ namespace Autons
 {
     void Skills::run()
     {
-        }
+        // start in a position where running the intake scores onto the alliance stake
+
+        pidDrivetrain.set_heading(180);
+        intakeMotors.setVelocity(intakeSpeed, percent);
+        intakeMotors.spin(forward);
+        wait(1000, msec);
+
+        intakeMotors.spin(reverse);
+        wait(500, msec);
+        pidDrivetrain.drive_distance(12); // tweak this number until the robot is perfectly centered on the start line
+    }
 }
