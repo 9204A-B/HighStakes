84d98f90b5c0bcaa0a044355b52aecbec7e673bf
KjEndurance <endurance.tem@gmail.com>
2025-01-21 17:41:10 -0600

    simplified prog skills to 35 points - reduced speed to 8V - 4 corners + 3 top rings + 6 rings - ending just yeets the last two goals (robot's positional accuracy WILL be lost)

--- HighStakes/src/autons/Skills.cpp
+++ HighStakes/src/autons/Skills.cpp
@@ -10,12 +10,17 @@ namespace Autons
 
         // start in a position where running the intake scores onto the alliance stake
 
-        pidDrivetrain.drive_max_voltage = 12;
-        pidDrivetrain.turn_max_voltage = 12;
+        float maxSpeed = 10;
+        float medSpeed = 8;
+        float lowSpeed = 6;
+
+        pidDrivetrain.drive_max_voltage = medSpeed;
+        pidDrivetrain.turn_max_voltage = medSpeed;
         intakeMotors.setVelocity(intakeSpeed, percent);
+        pidDrivetrain.set_heading(180);
 
         // score alliance stake and align for first goal
-        pidDrivetrain.set_heading(180);
+        antiJamEnable = false;
         intakeMotors.spin(forward);
         wait(750, msec);
         intakeMotors.stop();
@@ -23,12 +28,13 @@ namespace Autons
         wait(500, msec);
         intakeMotors.stop();
         pidDrivetrain.drive_distance(13.25);
+        antiJamEnable = true;
 
         // clamp goal 1
         pidDrivetrain.turn_to_angle(-90);
         // check here for proper alignment :)
 
-        pidDrivetrain.drive_max_voltage = 6;
+        pidDrivetrain.drive_max_voltage = lowSpeed;
         pidDrivetrain.drive_distance(-24);
         mobileGoalLock.set(true);
         wait(500, msec);
@@ -45,118 +51,83 @@ namespace Autons
         pidDrivetrain.turn_to_angle(0);
         pidDrivetrain.drive_distance(24);
 
-        pidDrivetrain.drive_max_voltage = 8;
-        pidDrivetrain.turn_max_voltage = 8;
+        pidDrivetrain.drive_max_voltage = lowSpeed;
+        pidDrivetrain.turn_max_voltage = lowSpeed;
 
-        pidDrivetrain.drive_distance(12);
         wait(500, msec);
+        pidDrivetrain.drive_distance(12);
         pidDrivetrain.drive_distance(-12);
         pidDrivetrain.turn_to_angle(180 + 45);
         intakeMotors.stop();
 
         // // drop first goal
-        // pidDrivetrain.drive_max_voltage = 12;
-        // pidDrivetrain.turn_max_voltage = 12;
+        pidDrivetrain.drive_max_voltage = medSpeed;
+        pidDrivetrain.turn_max_voltage = medSpeed;
 
-        // float goalDropDist1 = 33.9 - 7.5 - 10 - 3; // 7.5 is half the length of the robot, 10 is the length of the goal. tweak more if needed.
-        // pidDrivetrain.drive_distance(-1 * goalDropDist1);
-        // mobileGoalLock.set(false);
-        // wait(250, msec);
-        // pidDrivetrain.drive_distance(goalDropDist1);
+        float goalDropDist1 = 33.9 - 7.5 - 10 - 0; // 7.5 is half the length of the robot, 10 is the length of the goal. tweak more if needed.
+        pidDrivetrain.drive_distance(-1 * goalDropDist1);
+        mobileGoalLock.set(false);
+        wait(250, msec);
+        pidDrivetrain.drive_distance(goalDropDist1);
 
         // // clamp second goal
-        // pidDrivetrain.turn_to_angle(90);
-        // pidDrivetrain.drive_distance(-60);
+        pidDrivetrain.turn_to_angle(90);
+        pidDrivetrain.drive_distance(-60);
 
-        // pidDrivetrain.drive_max_voltage = 6; // dropping the speed so that the goal doesn't get knocked away
+        pidDrivetrain.drive_max_voltage = lowSpeed; // dropping the speed so that the goal doesn't get knocked away
 
-        // pidDrivetrain.drive_distance(-12);
-        // mobileGoalLock.set(true);
+        pidDrivetrain.drive_distance(-12);
+        mobileGoalLock.set(true);
 
-        // // 2 rings further away on the right
-        // pidDrivetrain.drive_max_voltage = 12;
+        // 2 rings further away on the right
+        pidDrivetrain.drive_max_voltage = medSpeed;
 
-        // pidDrivetrain.turn_to_angle(180);
-        // pidDrivetrain.drive_distance(24);
-        // pidDrivetrain.turn_to_angle(-90);
-        // pidDrivetrain.drive_distance(24);
+        pidDrivetrain.turn_to_angle(180);
+        pidDrivetrain.drive_distance(24);
+        pidDrivetrain.turn_to_angle(-90);
+        pidDrivetrain.drive_distance(24);
 
-        // // 2 rings on right corner
-        // pidDrivetrain.turn_to_angle(0);
-        // pidDrivetrain.drive_distance(24);
+        // 2 rings on right corner
+        pidDrivetrain.turn_to_angle(0);
+        pidDrivetrain.drive_distance(24);
 
-        // pidDrivetrain.drive_max_voltage = 8;
-        // pidDrivetrain.turn_max_voltage = 8;
+        pidDrivetrain.drive_max_voltage = lowSpeed;
+        pidDrivetrain.turn_max_voltage = lowSpeed;
 
-        // pidDrivetrain.drive_distance(12);
-        // wait(500, msec);
-        // pidDrivetrain.drive_distance(-12);
-        // pidDrivetrain.turn_to_angle(180 + 45);
-        // intakeMotors.stop();
+        wait(500, msec);
+        pidDrivetrain.drive_distance(12);
+        pidDrivetrain.drive_distance(-12);
+        pidDrivetrain.turn_to_angle(180 + 45);
+        intakeMotors.stop();
 
-        // // drop second goal
-        // pidDrivetrain.drive_max_voltage = 12;
-        // pidDrivetrain.turn_max_voltage = 12;
+        // drop second goal
+        pidDrivetrain.drive_max_voltage = medSpeed;
+        pidDrivetrain.turn_max_voltage = medSpeed;
 
-        // pidDrivetrain.turn_to_angle(90 + 45);
-        // pidDrivetrain.drive_distance(-1 * goalDropDist1);
-        // mobileGoalLock.set(false);
-        // wait(250, msec);
-        // pidDrivetrain.drive_distance(goalDropDist1);
+        pidDrivetrain.turn_to_angle(90 + 45);
+        pidDrivetrain.drive_distance(-1 * goalDropDist1);
+        mobileGoalLock.set(false);
+        wait(250, msec);
+        pidDrivetrain.drive_distance(goalDropDist1);
 
-        // // clamp third goal
-        // pidDrivetrain.turn_to_angle(180 - 14);
-        // pidDrivetrain.drive_distance(99);
-        // pidDrivetrain.turn_to_angle(-90);
+        // drive to right blue goal
+        pidDrivetrain.turn_to_angle(12.5);
+        pidDrivetrain.drive_distance(110.6 + 5); // tweak
+        mobileGoalLock.set(true);
+        wait(250, msec);
 
-        // pidDrivetrain.drive_max_voltage = 6;
+        // shove right blue goal into corner
+        pidDrivetrain.turn_to_angle(90);
+        float goalDropDist2 = 48 - 7.5 - 10 - 0;
+        pidDrivetrain.drive_max_voltage = maxSpeed;
+        pidDrivetrain.drive_distance(-1 * goalDropDist2);
+        mobileGoalLock.set(false);
+        wait(250, msec);
 
-        // pidDrivetrain.drive_distance(-24);
-        // mobileGoalLock.set(true);
-
-        // // 2 rings before going to corner
-        // pidDrivetrain.drive_max_voltage = 12;
-
-        // pidDrivetrain.turn_to_angle(-45);
-        // intakeMotors.spin(forward);
-        // pidDrivetrain.drive_distance(33.9);
-        // pidDrivetrain.drive_distance(33.9);
-        // pidDrivetrain.turn_to_angle(45);
-        // pidDrivetrain.drive_distance(33.9);
-        // pidDrivetrain.turn_to_angle(90);
-        // pidDrivetrain.drive_distance(24);
-
-        // // 2 in corner
-        // pidDrivetrain.turn_to_angle(180);
-        // pidDrivetrain.drive_distance(24);
-        // wait(500, msec);
-        // pidDrivetrain.turn_to_angle(90);
-        // pidDrivetrain.drive_distance(12);
-        // pidDrivetrain.drive_distance(-12);
-
-        // // drop third goal
-        // pidDrivetrain.turn_to_angle(-45);
-        // pidDrivetrain.drive_distance(-1 * goalDropDist1);
-        // mobileGoalLock.set(false);
-        // wait(250, msec);
-        // pidDrivetrain.drive_distance(goalDropDist1);
-
-        // // clamp final goal
-        // pidDrivetrain.drive_distance(48 + (24 - 6.9));
-        // pidDrivetrain.turn_to_angle(30);
-
-        // pidDrivetrain.drive_max_voltage = 6;
-
-        // pidDrivetrain.drive_distance(-13.9);
-        // mobileGoalLock.set(true);
-
-        // // drop final goal in corner
-        // pidDrivetrain.drive_max_voltage = 12;
-
-        // pidDrivetrain.turn_to_angle(90);
-        // pidDrivetrain.drive_distance(-1 * (24 - 10)); // tweak this value so that the robot isnt completely shoved into the wall
-        // mobileGoalLock.set(false);
-        // wait(250, msec);
-        // pidDrivetrain.drive_distance(24);
+        // shove left blue goal into corner
+        pidDrivetrain.drive_timeout = 5000;
+        pidDrivetrain.drive_distance(goalDropDist2 + 48 + goalDropDist2);
+        pidDrivetrain.drive_timeout = 3000;
+        pidDrivetrain.drive_distance(-48);
     }
 }
