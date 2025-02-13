d65054b152cb4de907220b3b78d66c50f6788e8c
KjEndurance <endurance.tem@gmail.com>
2025-02-13 08:36:22 -0600

    optimized colorsort

--- HighStakes/src/colorSort.cpp
+++ HighStakes/src/colorSort.cpp
@@ -35,6 +35,30 @@ void Sort()
     }
 }
 
+void colorSort() // i tried to make this cleaner
+{
+    colorSensor.setLightPower(50, percent);
+    while (true)
+    {
+        if (
+            (allianceSelect <= 1 &&
+            colorSortEnable && 
+            intakeMotors.voltage() > 0 &&
+            abs(colorSensor.hue() - blueHue) < error) ||
+            (allianceSelect == 2 &&
+            colorSortEnable && 
+            intakeMotors.voltage() > 0 &&
+            abs(colorSensor.hue() - redHue) < error)
+        )
+        {
+            wait(100, msec); // delay for the ring to travel further up the intake
+            intakeMotors.stop(brake);
+            wait(100, msec); // amount of time the intake stops for
+            intakeMotors.spin(forward);
+        }
+    }
+}
+
 void sortToggle()
 {
     colorSortEnable = !colorSortEnable;
