e6b609758e0bdc6031324c778d17e969bb71c520
Logan Dresel <coollogan876@gmail.com>
2025-04-07 14:08:09 -0500

    update config

--- HighStakes/src/robot-config.cpp
+++ HighStakes/src/robot-config.cpp
@@ -16,11 +16,11 @@ inertial Inertial = inertial(PORT10);
 motor_group leftMotors = motor_group(leftMotorA, leftMotorB, leftMotorC);
 motor_group rightMotors = motor_group(rightMotorA, rightMotorB, rightMotorC);
 smartdrive Drivetrain = smartdrive(leftMotors, rightMotors, Inertial, 82.55, 387.35, 304.8, mm, .6);
-Drive pidDrivetrain(TANK_TWO_ROTATION, leftMotors, rightMotors, PORT10, 3.25, .6, 360, PORT1, -PORT2, PORT3, -PORT4, PORT7, 2, 4, PORT8, 2, 4);
+Drive pidDrivetrain(TANK_TWO_ROTATION, leftMotors, rightMotors, PORT1, 3.25, .6, 360, PORT1, -PORT2, PORT3, -PORT4, PORT7, 2, 4, PORT8, 2, 0);
 float turnExp = 1; // MUST BE >= 1
 float turnScale = 0.8;
 
-digital_out mobileGoalLock = digital_out(Brain.ThreeWirePort.A);
+digital_out mobileGoalLock = digital_out(Brain.ThreeWirePort.D);
 distance goalDetect = distance(PORT2);
 bool locked = false;
 int clampDistance = 20;
@@ -32,7 +32,7 @@ bool intakeReverse = false;
 bool antiJamEnable = true;
 
 motor lb = motor(PORT3, ratio36_1, true);
-distance lbDistance = distance(PORT5);
+rotation lbRotation = rotation(PORT5);
 int lbSelect = 0;
 
 digital_out Doinker = digital_out(Brain.ThreeWirePort.C);
