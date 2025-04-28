098751994d80d0aaf34074da12626bd7deb74eff
Logan Dresel <coollogan876@gmail.com>
2025-04-28 13:30:09 -0500

    update temp warning

--- HighStakes/src/temp.cpp
+++ HighStakes/src/temp.cpp
@@ -1,23 +1,24 @@
 #include "vex.h"
 #include "array"
+#include <string>
 using namespace vex;
 
 void warning()
 {
-    motor* motors[] = {&leftMotorA, &leftMotorB, &leftMotorC, &rightMotorA, &rightMotorB, &rightMotorC, &intakeMotors};
-    // TODO: Probably need to update the ports
-    int ports[] = {11, 12, 13, 18, 19, 20, 21};
+    motor* motors[] = {&leftMotorA, &leftMotorB, &leftMotorC, &rightMotorA, &rightMotorB, &rightMotorC, &intakeMotors, &lb};
+    std::string names[] = {"left Motors", "left Motors", "left Motors", "right Motors", "right Motors", "right Motors", "intake", "lb"};
 
     while (true)
     {
-        for (int x = 0; x < 7; x++)
+        for (int x = 0; x < 8; x++)
         {
             if (motors[x]->temperature(celsius) > 50)
             {            
                 Controller.Screen.clearScreen();
                 Controller.Screen.setCursor(1, 1);
-                Controller.Screen.print("Motor port: %d", ports[x]);
-                Controller.Screen.print(", temp: %f", motors[x]->temperature(celsius));
+                Controller.Screen.print(names[x]);
+                Controller.Screen.setCursor(2, 1);
+                Controller.Screen.print("temp: %f", motors[x]->temperature(celsius));
                 Controller.rumble("...--");
                 wait(250, msec);
             }
